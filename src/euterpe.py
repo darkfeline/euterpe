794a3d661c1aab20f74595aa47fefe6ba2f48159
Allen Li <darkfeline@felesatra.moe>
2015-11-15 21:47:39 +0900

    Apply missing replaygain

--- src/euterpe.py
+++ src/euterpe.py
@@ -81,3 +81,6 @@ def main():
                     convert(enc, source, dest)
                 else:
                     logging.info('%s exists.', dest)
+
+    # Apply replaygain.
+    subprocess.call(['collectiongain', args.dest])
